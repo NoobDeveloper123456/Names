bdd3ad11c79063cba6bc61fffeea22f82ae7b0b8
NoobDeveloper123456 <42077177+NoobDeveloper123456@users.noreply.github.com>
2019-01-12 14:30:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,9 +43,52 @@ ch1.edit({
 color: '#FF0000',
 })  
 
-  setTimeout(guild3, 3 * 1e3);
+  setTimeout(guild3a, 3 * 1e3);
 } catch(e) {}}
 
+function guild3() {
+try {
+var guild = client.guilds.get('531130589869375488');
+  var role1 = guild.roles.get('533399617468104725');
+  var ch1 = role1
+
+ch1.edit({
+ 
+color: '#9400D3',
+})
+
+ch1.edit({
+ 
+color: '#4B0082',
+})
+
+ch1.edit({
+
+color: '#0000FF',
+})
+
+ch1.edit({
+
+color: '#00FF00',
+})
+  
+ch1.edit({
+
+color: '#FFFF00',
+})
+   
+ch1.edit({
+
+color: '#FF7F00',
+})   
+
+ch1.edit({
+
+color: '#FF0000',
+})  
+
+  setTimeout(guild3a, 3 * 1e3);
+} catch(e) {}}
 
 function guild4() {
 try {
@@ -102,6 +145,7 @@ client.once("ready", () => {
   guild5();
   guild4();
 guild3();
+  guild3a();
 
 });
 
